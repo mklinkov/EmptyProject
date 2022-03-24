7f0ee4aabe2471f921e99afd1a51bf97229a84a8
Maxim Linkov <mlinkov@yotateam.com>
2022-03-24 17:31:09 +0300

    Initial Commit

---
+++ EmptyProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmptyProject
+//
+//  Created by Maksim Linkov on 24.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
